ada09fc7dab2e24d103bd3117d8cad3e257ba87a
John Woolbright <jwoolbright23@gmail.com>
2023-06-28 12:58:41 -0500

    path variables branch

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,26 +2,38 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello(){
-        return "Hello, Spring!";
-    }
+    //handle request at path http://localhost:8080/hello
 
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
+
+    //handle request at path //http://localhost:8080/goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye(){
         return "Goodbye, Spring!";
     }
 
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
     @GetMapping("hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 }
