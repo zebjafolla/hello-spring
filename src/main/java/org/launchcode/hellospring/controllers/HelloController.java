a4949ae4b186794760c0bb68c81800b05410c48d
John Woolbright <jwoolbright23@gmail.com>
2023-07-10 14:50:28 -0500

    added thymeleaf dependancy, code for chapter 13 creating-templates

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,48 +3,36 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
 public class HelloController {
-    //handle request at path http://localhost:8080/hello
 
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!";
-//    }
-
-    //handle request at path //http://localhost:8080/goodbye
     @GetMapping("goodbye")
     @ResponseBody
-    public String goodbye(){
+    public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
+    // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name){
+    public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
-    //handle requests of the form http://localhost:8080/hello/LaunchCode
+    // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
     @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
+    public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
+    // /hello/form
     @GetMapping("form")
-    @ResponseBody
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action = 'hello' method = 'post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
 }
